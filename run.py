4dd7c2e7fcd75ca6f59ac59f06d45c4757b2a3cc
Shilu <89338633+shilu10@users.noreply.github.com>
2022-01-15 19:46:16 +0530

    final commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import argparse
-from app import Scraper
+from scraper import Scraper
 
 def parse() :
     parser = argparse.ArgumentParser(description = 'Exrtacting product info from amazon')
@@ -8,9 +8,11 @@ def parse() :
     parser.add_argument('-s', dest = "search", type =str, action = "store", default = False)
     return parser
 
+#parsing the value from the terminal input
 value = parse()
 value = value.parse_args()  
 
+#creating a Scraper instance
 laptops = Scraper(value.search  ,value.file_name , value.pageno)
 
 if __name__ == "__main__" :
